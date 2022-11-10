019bc67e9d8f4540a6289c50d2bbf2640b9d7362
MishynMykhailo <misham18092001@gmail.com>
2022-11-10 02:13:29 +0200

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div>About</div>
+      <div>
+        I am a full stack developer. I am focused on React, Node.js, JS, HTML5,
+        CSS3. For me, development, practice and work for the result are
+        important. I am looking for my first job to gain new experience and
+        develop my skills to a new level. Ready for new knowledge.
+      </div>
+    </>
+  );
 }
 
 export default About;
