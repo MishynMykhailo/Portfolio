2b99cae50ff23b63c6c0f8903e5d4a16648d60f1
MishynMykhailo <misham18092001@gmail.com>
2023-07-21 12:42:50 +0300

    added filter for portfolio section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,14 +1,94 @@
 import * as React from 'react';
+import { useState } from 'react';
 import s from './Portfolio.module.scss';
 import images from '../../images/portfolio';
 function Portfolio() {
+  const [testTaskFilter, setTestTaskFilter] = useState(Boolean);
+  const [frontEndFilter, setFrontEndFilter] = useState(Boolean);
+  const [backEndFilter, setBackEndFilter] = useState(Boolean);
+  const filterTag = ['testTask', 'frontEnd', 'backEnd'];
+  const handlerFilter = e => {
+    const name = e.currentTarget.name;
+    switch (name) {
+      case 'testTask':
+        setTestTaskFilter(!testTaskFilter);
+        setBackEndFilter(false);
+        setFrontEndFilter(false);
+        break;
+      case 'frontEnd':
+        setFrontEndFilter(!frontEndFilter);
+        setTestTaskFilter(false);
+        setBackEndFilter(false);
+        break;
+      case 'BackEnd':
+        setBackEndFilter(!backEndFilter);
+        setFrontEndFilter(false);
+        setTestTaskFilter(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const filteredImages = images.filter(image => {
+    return (
+      (testTaskFilter && image.filter === 'testtask') ||
+      (frontEndFilter && image.filter === 'frontend') ||
+      (backEndFilter && image.filter === 'backend') ||
+      (testTaskFilter === false &&
+        frontEndFilter === false &&
+        backEndFilter === false)
+    );
+  });
+
   return (
     <>
-      <div className={s.div} id="portfolio">
+      <div id="portfolio">
         <h2 className={s.h2}>Portfolio</h2>
-        <ul className={s.ul}>
-          {images.map(image => {
-            return (
+        <div className={s.portfolioFilter}>
+          <button
+            className={s.btnFilter}
+            name="testTask"
+            id="testTask"
+            onClick={e => handlerFilter(e)}
+            style={
+              testTaskFilter
+                ? { backgroundColor: 'var(--primary-bg-color)' }
+                : {}
+            }
+          >
+            Test Task
+          </button>
+          <button
+            className={s.btnFilter}
+            name="frontEnd"
+            id="frontEnd"
+            onClick={e => handlerFilter(e)}
+            style={
+              frontEndFilter
+                ? { backgroundColor: 'var(--primary-bg-color)' }
+                : {}
+            }
+          >
+            Front-End
+          </button>
+          <button
+            className={s.btnFilter}
+            name="BackEnd"
+            id="BackEnd"
+            onClick={e => handlerFilter(e)}
+            style={
+              backEndFilter
+                ? { backgroundColor: 'var(--primary-bg-color)' }
+                : {}
+            }
+          >
+            BackEnd
+          </button>
+        </div>
+        <ul className={`${s.ul} ${s.div}`}>
+          {filteredImages.length > 0 ? (
+            filteredImages.map(image => (
               <li key={image.name} className={s.li}>
                 <img
                   className={s.img}
@@ -20,9 +100,9 @@ function Portfolio() {
                 />
                 <div className={s.overlay}>
                   <div>
-                    <div class={s.overlayName}>{image.name}</div>
-                    <div class={s.overlayDescription}>{image.desc}</div>
-                    <div class={s.overlayLink}>
+                    <div className={s.overlayName}>{image.name}</div>
+                    <div className={s.overlayDescription}>{image.desc}</div>
+                    <div className={s.overlayLink}>
                       {image.demo.length > 0 && (
                         <a target="blank" href={image.demo} className={s.a}>
                           <p className={s.p}>Demo</p>
@@ -36,8 +116,10 @@ function Portfolio() {
                   </div>
                 </div>
               </li>
-            );
-          })}
+            ))
+          ) : (
+            <h2 className={s.errorCard}>Not found</h2>
+          )}
         </ul>
       </div>
     </>
--- src/images/portfolio/index.js
+++ src/images/portfolio/index.js
@@ -9,39 +9,12 @@ import PizzaStyle from '../portfolio/PizzaStyle.jpg';
 
 const images = [
   {
-    name: 'bookreaderBackEnd',
-    img: bookreaderBackEnd,
-    desc: 'NODE.JS, MONGODB, EXPRESS, SWAGGER',
-    demo: '',
-    code: 'https://github.com/nataliakif/BR-backend',
-  },
-  {
     name: 'contactBook',
     img: contactBook,
     desc: 'REACT/REDUX, REACT-ROUTER, MATERIAL-UI, REDUX-THUNK',
     demo: 'https://mishynmykhailo.github.io/goit-react-hw-08-phonebook/',
     code: 'https://github.com/MishynMykhailo/goit-react-hw-08-phonebook',
-  },
-  {
-    name: 'filmoteka',
-    img: filmoteka,
-    desc: 'HTML, CSS, SASS, JS',
-    demo: 'https://nataliakif.github.io/project-filmoteka/',
-    code: 'https://github.com/nataliakif/project-filmoteka',
-  },
-  {
-    name: 'iceCream',
-    img: iceCream,
-    desc: 'HTML, CSS, SASS',
-    demo: 'https://nataliakif.github.io/IceCream-project8/',
-    code: 'https://github.com/nataliakif/IceCream-project8',
-  },
-  {
-    name: 'webStudio',
-    img: webStudio,
-    desc: 'HTML, CSS, SASS',
-    demo: 'https://mishynmykhailo.github.io/goit-markup-hw-08/',
-    code: 'https://github.com/MishynMykhailo/goit-markup-hw-08',
+    filter: 'frontend',
   },
   {
     name: 'laravel-book-library',
@@ -49,13 +22,7 @@ const images = [
     desc: 'PHP, LARAVEL, COMPOSER',
     demo: '',
     code: 'https://github.com/MishynMykhailo/Laravel-book-library',
-  },
-  {
-    name: 'parser-sites',
-    img: ParserSite,
-    desc: 'NODE.JS, PUPPETEER, AXIOS',
-    demo: '',
-    code: 'https://github.com/MishynMykhailo/parser-sites',
+    filter: 'frontend',
   },
   {
     name: 'pizza-style-testtask',
@@ -63,6 +30,47 @@ const images = [
     desc: 'REACT/REDUX, REACT-ROUTER-DOM, PERSISTENT-STORAGE',
     demo: 'https://mishynmykhailo.github.io/pizza-style-testtask/',
     code: 'https://github.com/MishynMykhailo/pizza-style-testtask',
+    filter: 'frontend',
+  },
+  {
+    name: 'filmoteka',
+    img: filmoteka,
+    desc: 'HTML, CSS, SASS, JS',
+    demo: 'https://nataliakif.github.io/project-filmoteka/',
+    code: 'https://github.com/nataliakif/project-filmoteka',
+    filter: 'frontend',
+  },
+  {
+    name: 'iceCream',
+    img: iceCream,
+    desc: 'HTML, CSS, SASS',
+    demo: 'https://nataliakif.github.io/IceCream-project8/',
+    code: 'https://github.com/nataliakif/IceCream-project8',
+    filter: 'frontend',
+  },
+  {
+    name: 'webStudio',
+    img: webStudio,
+    desc: 'HTML, CSS, SASS',
+    demo: 'https://mishynmykhailo.github.io/goit-markup-hw-08/',
+    code: 'https://github.com/MishynMykhailo/goit-markup-hw-08',
+    filter: 'frontend',
+  },
+  {
+    name: 'parser-sites',
+    img: ParserSite,
+    desc: 'NODE.JS, PUPPETEER, AXIOS',
+    demo: '',
+    code: 'https://github.com/MishynMykhailo/parser-sites',
+    filter: 'backend',
+  },
+  {
+    name: 'bookreaderBackEnd',
+    img: bookreaderBackEnd,
+    desc: 'NODE.JS, MONGODB, EXPRESS, SWAGGER',
+    demo: '',
+    code: 'https://github.com/nataliakif/BR-backend',
+    filter: 'backend',
   },
 ];
 export default images;
