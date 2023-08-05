0d6e37f4737e9a3664f11fd54a91c2dd83fba84c
MishynMykhailo <misham18092001@gmail.com>
2023-08-06 02:53:33 +0300

    added new projects

--- src/images/portfolio/index.js
+++ src/images/portfolio/index.js
@@ -6,8 +6,25 @@ import webStudio from '../portfolio/WebStudio.jpg';
 import LaravelLibrary from '../portfolio/LaravelLibrary.jpg';
 import ParserSite from '../portfolio/ParserSite.jpg';
 import PizzaStyle from '../portfolio/PizzaStyle.jpg';
+import TestShortUrlProject from './TestShortUrlProject.jpg';
+import TestTaskRadency3 from './TestTaskRadency3.jpg';
+import TestTaskRadency2 from './TestTaskRadency2.jpg';
+import TestTaskRadency1 from './TestTaskRadency1.jpg';
+import TeskTaskTovaruIzUSA from './TeskTaskTovaruIzUSA.jpg';
+import GowebTesttask from './GowebTesttask.jpg';
+import PhonebookTesttask from './PhonebookTesttask.jpg';
+import CleveroadTesttask from './CleveroadTesttask.jpg';
+import VueProject from './VueProject.jpg';
 
 const images = [
+  {
+    name: 'VueProject',
+    img: VueProject,
+    desc: 'VUE, VUEX',
+    demo: 'https://mishynmykhailo.github.io/vue-project/',
+    code: 'https://github.com/MishynMykhailo/vue-project',
+    filter: 'frontend',
+  },
   {
     name: 'contactBook',
     img: contactBook,
@@ -22,14 +39,6 @@ const images = [
     desc: 'PHP, LARAVEL, COMPOSER',
     demo: '',
     code: 'https://github.com/MishynMykhailo/Laravel-book-library',
-    filter: 'frontend',
-  },
-  {
-    name: 'pizza-style-testtask',
-    img: PizzaStyle,
-    desc: 'REACT/REDUX, REACT-ROUTER-DOM, PERSISTENT-STORAGE',
-    demo: 'https://mishynmykhailo.github.io/pizza-style-testtask/',
-    code: 'https://github.com/MishynMykhailo/pizza-style-testtask',
     filter: 'frontend',
   },
   {
@@ -72,5 +81,78 @@ const images = [
     code: 'https://github.com/nataliakif/BR-backend',
     filter: 'backend',
   },
+  {
+    name: 'TestShortUrlProject',
+    img: TestShortUrlProject,
+    desc: 'REACT, DOCKER, POSTGRESQL, SEQUELIZE, NESTJS',
+    demo: '',
+    code: 'https://github.com/MishynMykhailo/test-short-url-project',
+    filter: 'testtask',
+  },
+  {
+    name: 'TestTaskRadency3',
+    img: TestTaskRadency3,
+    desc: 'TYPESCRIPT, DOCKER, POSTGRESQL, SEQUELIZE, NESTJS',
+    demo: '',
+    code: 'https://github.com/MishynMykhailo/test-task-radency-3',
+    filter: 'testtask',
+  },
+  {
+    name: 'TestTaskRadency2',
+    img: TestTaskRadency2,
+    desc: 'TYPESCRIPT, REACT, REDUX-TOOLKIT',
+    demo: 'https://mishynmykhailo.github.io/test-task-radency-2/',
+    code: 'https://github.com/MishynMykhailo/test-task-radency-2',
+    filter: 'testtask',
+  },
+  {
+    name: 'TestTaskRadency1',
+    img: TestTaskRadency1,
+    desc: 'JS, HTML, CSS',
+    demo: 'https://mishynmykhailo.github.io/test-task-radency-1/',
+    code: 'https://github.com/MishynMykhailo/test-task-radency-1',
+    filter: 'testtask',
+  },
+  {
+    name: 'TeskTaskTovaruIzUSA',
+    img: TeskTaskTovaruIzUSA,
+    desc: 'JS, HTML, CSS',
+    demo: '',
+    code: 'https://github.com/MishynMykhailo/tesk-task-TovaruIzUSA',
+    filter: 'testtask',
+  },
+  {
+    name: 'GowebTesttask',
+    img: GowebTesttask,
+    desc: 'JS, HTML, CSS',
+    demo: 'https://goweb-test-mishynmykhailo.netlify.app/',
+    code: 'https://github.com/MishynMykhailo/goweb-testtask',
+    filter: 'testtask',
+  },
+  {
+    name: 'PhonebookTesttask',
+    img: PhonebookTesttask,
+    desc: 'JS, HTML, CSS',
+    demo: 'https://mishynmykhailo.github.io/phonebook-testtask/',
+    code: 'https://github.com/MishynMykhailo/phonebook-testtask',
+    filter: 'testtask',
+  },
+  {
+    name: 'PizzaStyleTesttask',
+    img: PizzaStyle,
+    desc: 'REACT/REDUX, REACT-ROUTER-DOM, PERSISTENT-STORAGE',
+    demo: 'https://mishynmykhailo.github.io/pizza-style-testtask/',
+    code: 'https://github.com/MishynMykhailo/pizza-style-testtask',
+    filter: 'testtask',
+  },
+  {
+    name: 'CleveroadTesttask',
+    img: CleveroadTesttask,
+    desc: 'REACT, AXIOS, REACT-GOOGLE-MAPS-API',
+    demo: '',
+    code: 'https://github.com/MishynMykhailo/Cleveroad-testTask',
+    filter: 'testtask',
+  },
 ];
+
 export default images;
